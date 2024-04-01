b018ea0e19a484b8b20db57804223153e16629fc
Chris Diggle <chrisdiggle8@outlook.com>
2024-04-01 12:12:21 +0000

    Add class component learning module

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
+// import FunctionalGreeting from './components/FunctionalGreeting';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      {/* <FunctionalGreeting /> */}
+      <StatefulGreeting greeting="Im a stateful class component!" />
     </div>
   );
 }
