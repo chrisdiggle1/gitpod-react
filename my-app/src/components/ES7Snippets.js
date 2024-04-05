82bfe1e52ab50e3296fa1aad31d305297fe0dd1b
Chris Diggle <chrisdiggle8@outlook.com>
2024-04-05 19:27:39 +0000

    ES7 snippets for use in React

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,65 @@
+// NOT A COMPONENT - JUST SNIPPET NOTES
+
+//rfce - creates and auto export a new functional componment.
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+//rafce - same as above but an arrow function
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+//rce - Creates and exports a class component.
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+
+//rcosnt - Creates a constructor
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      
+//   }
+// }
+
+
+
+//clg - to log something to the console
+// console.log(first)
+
+
+
+// clo - if you want to log an object  along with its name as a string.
+// console.log('object', object)
