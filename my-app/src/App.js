4f774e01745500e62a07878264791725597bd8f9
Chris Diggle <chrisdiggle8@outlook.com>
2024-04-10 11:03:11 +0000

    The use state hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SearchBar';
+// import SearchBar from './components/SearchBar';
 // import FunctionalGreeting from './components/FunctionalGreeting';
-import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import ControlledFormHooks from './components/ControlledFormHooks';
+// import UseStateWithArrays from './components/UseStateWithArrays';
+// import UseStateWithObjects from './components/UseStateWithObjects';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting /> */}
       {/* <StatefulGreeting greeting="Im a stateful class component!" /> */}
-      <SearchBar />
+      {/* <SearchBar /> */}
+      {/* <ControlledFormHooks /> */}
+      {/* <UseStateWithArrays /> */}
+      {/* <UseStateWithObjects /> */}
     </div>
   );
 }
