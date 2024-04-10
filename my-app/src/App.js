ae8eb39af4f8617d6b3754e95b83caf6026af85e
Chris Diggle <chrisdiggle8@outlook.com>
2024-04-10 08:46:15 +0000

    Search bar challenge in forms module

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar';
 // import FunctionalGreeting from './components/FunctionalGreeting';
 import StatefulGreeting from './components/StatefulGreeting';
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting /> */}
-      <StatefulGreeting greeting="Im a stateful class component!" />
+      {/* <StatefulGreeting greeting="Im a stateful class component!" /> */}
+      <SearchBar />
     </div>
   );
 }
--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,60 +1,60 @@
-import React from "react";
+// import React from "react";
 
-class StatefulGreeting extends React.Component {
+// class StatefulGreeting extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          introduction: "Hello!",
-          buttonText: "Exit",
-          count: 0,
-        };
-      }
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//           introduction: "Hello!",
+//           buttonText: "Exit",
+//           count: 0,
+//         };
+//       }
     
-      handleClick() {
-        this.setState((prevState, prevProps) => {
-          console.log("Previous State:", prevState);
-          console.log("Previous Props:", prevProps);
-          return {
-            introduction:
-              prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
-            buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
-          };
-        });
-      }
+//       handleClick() {
+//         this.setState((prevState, prevProps) => {
+//           console.log("Previous State:", prevState);
+//           console.log("Previous Props:", prevProps);
+//           return {
+//             introduction:
+//               prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+//             buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+//           };
+//         });
+//       }
     
-      increment() {
-        this.setState((prevState, prevProps) => {
-          console.log("Previous State:", prevState);
-          console.log("Previous Props:", prevProps);
-          return {
-            count: prevState.count + 1,
-          };
-        });
-      }
+//       increment() {
+//         this.setState((prevState, prevProps) => {
+//           console.log("Previous State:", prevState);
+//           console.log("Previous Props:", prevProps);
+//           return {
+//             count: prevState.count + 1,
+//           };
+//         });
+//       }
     
-      incrementFive() {
-        this.increment();
-        this.increment();
-        this.increment();
-        this.increment();
-        this.increment();
-      }
+//       incrementFive() {
+//         this.increment();
+//         this.increment();
+//         this.increment();
+//         this.increment();
+//         this.increment();
+//       }
     
-      render() {
-        return (
-          <div>
-            <h1>
-              {this.state.introduction} {this.props.greeting}
-            </h1>
-            <button onClick={() => this.handleClick()}>
-              {this.state.buttonText}
-            </button>
-            <button onClick={() => this.increment()}>Increment</button>
-            <p>You've clicked {this.state.count} times</p>
-          </div>
-        );
-      }
-    }
+//       render() {
+//         return (
+//           <div>
+//             <h1>
+//               {this.state.introduction} {this.props.greeting}
+//             </h1>
+//             <button onClick={() => this.handleClick()}>
+//               {this.state.buttonText}
+//             </button>
+//             <button onClick={() => this.increment()}>Increment</button>
+//             <p>You've clicked {this.state.count} times</p>
+//           </div>
+//         );
+//       }
+//     }
 
-export default StatefulGreeting;
+// export default StatefulGreeting;
